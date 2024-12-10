76aed5f9614f303fddc38edabe787b0287cb7e44
Wepngong Shalom <wepngongshalom23@gmail.com>
2024-12-10 05:14:03 +0100

    correction of variable names

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,9 +1,9 @@
-mincome = int(input("Enter your monthly income: "))
-mexpenses = int(input("Enter your total monthly expenses: "))
+monthly_income = int(input("Enter your monthly income: "))
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
-msavings = mincome - mexpenses
+monthly_savings = monthly_income - monthly_expenses
 rate = 0.05
-projected_annual_savings = (msavings * 12 + (msavings * 12 * rate))
+projected_annual_savings = (monthly_savings * 12 + (monthly_savings * 12 * rate))
 
-print("Your monthly savings are $" + str(msavings) + ".")
+print("Your monthly savings are $" + str(monthly_savings) + ".")
 print("Projected savings after 1 year, with savings, is $" + str(projected_annual_savings) + ".")
