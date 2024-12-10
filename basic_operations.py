0f42c393b58ded69dbe553a217552c9ae2529ebe
Wepngong Shalom <wepngongshalom23@gmail.com>
2024-12-10 04:23:55 +0100

    This is a file on working with basic math operations in python

---
+++ basic_operations.py
@@ -0,0 +1,8 @@
+number1 = 10
+number2 = 5
+result = number1 + number2
+print("Addition of " + str(number1) + " and " + str(number2) + " is " + str(result))
+result = number1 - number2
+print("Subtraction of " + str(number1) + " and " + str(number2) + " is " + str(result))
+result = number1 * number2
+print("Multiplication of " + str(number1) + " and " + str(number2) + " is " + str(result))
