e18ea0539d9d7a818c6483c8dac58bc99199e87c
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-01-12 22:00:15 +0100

    temp conversion tool

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,13 +1,13 @@
-CELSIUS_TO_FAHRENHEIT_FACTOR = 9/5
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5/9
-
-
-def convert_to_celsius(fahrenheit):
-    return (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
+CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 
 
 def convert_to_fahrenheit(celsius):
-    return (celsius+32)*CELSIUS_TO_FAHRENHEIT_FACTOR
+    return (celsius + 32) * CELSIUS_TO_FAHRENHEIT_FACTOR
+
+
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
 
 
 try:
