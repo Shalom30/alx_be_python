c9379a437990de33c4f6ed292d9b11c61d4b5eb0
Wepngong Shalom <wepngongshalom23@gmail.com>
2025-01-19 21:54:59 +0100

    file adjusting

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -9,9 +9,9 @@ class BankAccount:
     def withdraw(self, amount):
         if self.account_balance >= amount:
             self.account_balance -= amount
-            return self.account_balance
+            return True
         else:
-            print("Insufficient funds.")
+            return False
 
     def display_balance(self):
         print(f"Current Balance: $[{self.account_balance}]")
